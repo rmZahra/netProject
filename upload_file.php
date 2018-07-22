e37f6ee63ca4a2e8600dc3c0801186bcec473719
rmZahra <40898865+rmZahra@users.noreply.github.com>
2018-07-22 11:24:26 +0430

    Add files via upload

---
+++ upload_file.php
@@ -0,0 +1,34 @@
+<?php
+$allowedExts = array("gif", "jpeg", "jpg", "png" , "txt", "pdf");
+$temp = explode(".", $_FILES["file"]["name"]);
+$extension = end($temp);
+if ((($_FILES["file"]["type"] == "image/gif")
+|| ($_FILES["file"]["type"] == "image/jpeg")
+|| ($_FILES["file"]["type"] == "image/jpg")
+|| ($_FILES["file"]["type"] == "image/png")
+|| ($_FILES["file"]["type"] == "text/plain")
+|| ($_FILES["file"]["type"] == "application/msword")
+|| ($_FILES["file"]["type"] == "application/vnd.ms-excel")
+)
+&& ($_FILES["file"]["size"] < 20000)
+&& in_array($extension, $allowedExts)){
+    if ($_FILES["file"]["error"] > 0){
+        echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
+    }else{
+        echo "Upload: " . $_FILES["file"]["name"] . "<br>";
+        echo "Type: " . $_FILES["file"]["type"] . "<br>";
+        echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
+        echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br>";
+
+        if (file_exists("upload/" . $_FILES["file"]["name"])){
+            echo $_FILES["file"]["name"] . " already exists. ";
+        }else{
+            move_uploaded_file($_FILES["file"]["tmp_name"],
+            "upload/" . $_FILES["file"]["name"]);
+            echo "Stored in: " . "upload/" . $_FILES["file"]["name"];
+        }
+    }
+}else{
+    echo "Invalid file";
+}
+?>
