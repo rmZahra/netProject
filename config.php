2ecdc35f31e84ff56fe80a02f6f82a07cb9fa3fa
rmZahra <40898865+rmZahra@users.noreply.github.com>
2018-07-22 20:10:22 +0430

    Add files via upload

---
+++ config.php
@@ -0,0 +1,21 @@
+﻿<?PHP
+	$hostname='localhost';
+	$dbname='project';
+	$dbpass='';
+	$dbuser='root';
+	$con=mysql_connect($hostname,$dbuser,$dbpass);
+
+		if(!$con)
+			{
+				die ('could not connect!' . mysql_error()); 
+			}
+			mysql_set_charset('UTF-8',$con);
+			mysql_select_db($dbname, $con); 
+			
+
+		
+	
+	$adminuser="admin";
+	$adminpass="123";
+		
+?>
---
+++ login.php
@@ -0,0 +1,46 @@
+<?php
+	session_start(); 
+	if(isset($_POST['username']) && isset($_POST['password']))
+	{
+		include 'config.php';
+	
+	if(($_POST['username']==$adminuser) && ($_POST['password']==$adminpass))
+	{
+		$_SESSION['user']=$_POST['username'];
+		
+	}
+	
+	}
+?>
+<html>
+<head>
+<meta charset="utf-8">
+<link  rel="stylesheet" type="text/css" href="style.css">
+<body>
+
+<div class="m">
+
+<div class="help">
+<br><p> دسترسی به مستند مربوط به پروژه با استفاده از نام کاربری و رمز عبوری که از قبل برای مدرس کلاس تعریف شده است انجام می شود </p>
+</body>
+</head>
+
+<div class="help2">
+
+	<body bgcolor="#B5A5D5">
+		<form action="document.php" method=POST >
+		
+		<div class="field">
+		<b>username	<input type="text" name="username">
+		</div>
+		
+		<b>password	<input type="password" name="password">
+		<div class="field">
+		<br>	<input type="submit" value="enter">
+			</div>
+</div>
+</div><!--end m-->
+<
+		</form>
+	</body>
+</html>
